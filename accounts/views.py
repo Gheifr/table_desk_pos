e5fd3de522cfbef776a06b34c4e8ca24b2cd7d63
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-07 11:55:28 +0200

    Update views to be accessible for logged in users

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
@@ -7,11 +7,12 @@ from django.views import generic
 from accounts.models import Employee
 
 
+@login_required
 def index(request: HttpRequest):
     return render(request, "account/index.html")
 
 
-class StaffDetailsView(generic.DetailView):
+class StaffDetailsView(LoginRequiredMixin, generic.DetailView):
     model = Employee
     queryset = Employee.objects.filter(is_active=True).prefetch_related("tables")
     template_name = "account/staff_detail.html"
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
 
+
+@login_required
 def index(request: HttpRequest):
     return render(request, "menu/index.html")
-
