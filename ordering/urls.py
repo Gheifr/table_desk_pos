59bf6f385ae8a5416b40b6cc8c5eec55dc4411fb
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-08 13:33:55 +0200

    Add orders list representation, update base

--- ordering/urls.py
+++ ordering/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ordering.views import index
+from ordering.views import index, OrderDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("<int:pk>", OrderDetailView.as_view(), name="order-detail"),
 ]
 
 app_name = "orders"
--- ordering/views.py
+++ ordering/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import generic
+
+from ordering.models import Order
 
 
 @login_required
 def index(request: HttpRequest):
-    return render(request, "ordering/index.html")
+    return render(request, "ordering/index.html", {"orders": Order.objects.all()})
+
+
+class OrderDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Order
