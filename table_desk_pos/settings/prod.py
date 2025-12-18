dbf5442a9f78495ab7d8cd58d93b8c621ec58459
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-18 16:45:48 +0200

    Update prod config file

--- table_desk_pos/settings/prod.py
+++ table_desk_pos/settings/prod.py
@@ -2,9 +2,6 @@ from urllib.parse import urlparse, parse_qsl
 
 from .base import *
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = False
 
 ALLOWED_HOSTS = []
@@ -13,11 +10,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-# Replace the DATABASES section of your settings.py with this
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 DATABASES = {
