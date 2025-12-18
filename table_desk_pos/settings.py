1cb35043a5d91fd941d8646cc8360e6237da12b0
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-18 14:21:32 +0200

    Update settings and requirements

--- table_desk_pos/settings.py
+++ table_desk_pos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0()z4%%dij-^-4xc24!(556z0he@c3v3ic_k-)r75dn$16+3$%"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-0()z4%%dij-^-4xc24!(556z0he@c3v3ic_k-)r75dn$16+3$%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
